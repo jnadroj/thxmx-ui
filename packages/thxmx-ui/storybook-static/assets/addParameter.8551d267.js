ca26fcd5ac019f63760ed060a23a616fa8157096
Jordan Jaramillo <rjaramilloby@gmail.com>
2022-09-20 10:34:43 -0500

    fix: remove the folder of storybook-static

--- packages/thxmx-ui/storybook-static/assets/addParameter.8551d267.js
+++
@@ -1,2 +0,0 @@
-var a={backgrounds:{grid:{cellSize:20,opacity:.5,cellAmount:5},values:[{name:"light",value:"#F8F8F8"},{name:"dark",value:"#333333"}]}};export{a as parameters};
-//# sourceMappingURL=addParameter.8551d267.js.map
